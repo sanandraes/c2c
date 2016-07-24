5feebda6ebf7974c32dde68e7f826d7e549c1b58
sanandraes <zachariahjaffe@yahoo.com>
2016-07-24 09:59:09 -0400

    ghetto rendering and stuff

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame, time, map
 pygame.init()
 
-size = width, height = 600, 400
-screen = pygame.display.set_mode(size)
+SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 600
+TILE_SIZE = 10
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 image = pygame.image.load("image.bmp")
 image_rect = image.get_rect()
 
+def render_map(activemap):
+	for x in range(activemap.width):
+		for y in range(activemap.height):
+			pygame.draw.rect(screen, activemap.maparray[x][y].terrain.color, (x*activemap.side_length, y*activemap.side_length, activemap.side_length, activemap.side_length))
+
+testmap = map.Map(SCREEN_WIDTH/TILE_SIZE,SCREEN_HEIGHT/TILE_SIZE,TILE_SIZE)
+
 while 1:
-	screen.blit(image, image_rect)
+	render_map(testmap)
 	pygame.display.flip()
--- map.py
+++ map.py
@@ -4,14 +4,19 @@ class Terrain:
 	def __init__(self, colour):
 		self.color = colour
 
+grassland = Terrain(pygame.Color('#6EA028'))
+		
 class Tile:
-	def __init__(self, terrain, explored = True):
+	def __init__(self, terrain = grassland, explored = True):
 		#add some idea of terrain, movement blocking, improvements, culture... ???
 		self.explored = explored
 		self.terrain = terrain
 
 class Map:
-	def __init__(self, height, width):
-		map = [[Tile() for y in range(height)] for x in range(width)]
+	def __init__(self, width, height, side_length = 10):
+		self.width = width
+		self.height = height
+		self.side_length = side_length
+		self.maparray = [[Tile(grassland) for y in range(height)] for x in range(width)]
 		
-grassland = Terrain(pygame.Color('#6EA028'))
+
