8cff42281c6b2af2270ce528d221ee77a53dc58b
sanandraes <zachariahjaffe@yahoo.com>
2016-07-24 04:14:23 -0400

    goals 1 and 2

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import pygame
+import pygame, time
 pygame.init()
+
+size = width, height = 600, 400
+screen = pygame.display.set_mode(size)
+image = pygame.image.load("image.bmp")
+image_rect = image.get_rect()
+
+screen.blit(image, image_rect)
+pygame.display.flip()
+
+time.sleep(10)
