5827823473eb2cbee8006c3909ed3f024096165f
sanandraes <zachariahjaffe@yahoo.com>
2016-07-28 17:38:24 -0400

    commenting

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import pygame, settings, time, map
 from pygame.locals import * #to make namespace calls not suck
 pygame.init()
 
-
+#init screen
 screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
 
+#init a map
 testmap = map.Map(100, 100, settings.TILE_SIZE)
-testmap.ready_surface()
 
 def handle_input():
+	
+	#if the player quit, then quit...
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			quit()
 	
+	#collect the current keyboard/mouse state
 	keyspressed = pygame.key.get_pressed()
+	mousepos = pygame.mouse.get_pos()
 	
+	#map arrow keys to camera movement
 	if keyspressed[K_DOWN]:
 		testmap.move_camera(0,1)
 	elif keyspressed[K_UP]:
@@ -23,10 +28,19 @@ def handle_input():
 		testmap.move_camera(-1,0)
 	elif keyspressed[K_RIGHT]:
 		testmap.move_camera(1,0)
-	
-			
+		
+	#edgepanning code, maybe tighten this up: diagonals are not friendly
+	if mousepos[0] < 3:
+		testmap.move_camera(-1,0)
+	elif mousepos[0] > settings.SCREEN_WIDTH - 4:
+		testmap.move_camera(1,0)
+	if mousepos[1] < 3:
+		testmap.move_camera(0,-1)
+	elif mousepos[1] > settings.SCREEN_HEIGHT - 4:
+		testmap.move_camera(0,1)
+		
+#game loop		
 while 1:
-	
 	handle_input()
 	testmap.render(screen)
 	pygame.display.flip()
--- map.py
+++ map.py
@@ -1,25 +1,31 @@
 import pygame, random, settings
 
+#def terrain, contains info about rendering to map and game mechanics
 class Terrain:
 	def __init__(self, colour):
 		self.color = colour
 
+#define some placeholder terrains for testing, eventually this should be part of an init somewhere
 grassland = Terrain(pygame.Color('#6EA028'))
 ocean = Terrain(pygame.Color('#0000cc'))
 		
+#a placeholder map generation basically, to be called in map init
 def terrain_randomizer():
 	p = random.random()
 	if p > 0.5: return grassland
 	else: return ocean
-		
+
+#a cell on a map, will contain a terrain object, and info about units/cities/cultures/workers whatever
 class Tile:
 	def __init__(self, terrain, explored = True):
 		#add some idea of terrain, movement blocking, improvements, culture... ???
 		self.explored = explored
 		self.terrain = terrain
 
+#map, contains an array of tiles and some infrastructure for rendering/viewporting
 class Map:
 	def __init__(self, width, height, side_length = 10):
+		#some important constants/values 
 		self.width = width
 		self.height = height
 		self.side_length = side_length
@@ -29,18 +35,25 @@ class Map:
 		#placeholder map generation
 		self.maparray = [[Tile(terrain_randomizer()) for y in range(height)] for x in range(width)]
 		
+		#create important surfaces for viewport and rendering
 		self.mapsurface = pygame.Surface((side_length*width, side_length*height))
 		self.camerarect = pygame.Rect((self.camera_x,self.camera_y),(settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
 		
+		#draw the mapsurface of which the viewport is a subset
+		self.ready_surface()
+
+	#creates a graphical interpretaion of self.maparray on self.mapsurface
 	def ready_surface(self):
 		for x in range(self.width):
 			for y in range(self.height):
 				pygame.draw.rect(self.mapsurface, self.maparray[x][y].terrain.color, (x*self.side_length, y*self.side_length, self.side_length, self.side_length))
 
-		
+	#send a subset of self.mapsurface (defined by self.camrerect) to a surface 'dest', (this is all but hardcoded as main.screen, that's probably bad...)
 	def render(self, dest):
 		dest.blit(self.mapsurface, (0,0), self.camerarect)
 		
+	#move the camera by dx and dy unless at an edge
+	#this sounds inefficient to me, think about asigning direclty instead of through a variable
 	def move_camera(self, dx, dy):
 		self.camera_x += dx
 		self.camera_y += dy
